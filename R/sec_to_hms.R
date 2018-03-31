6784e352d23e27897fb083fd3d5c83d2da003ecf
Alan Rincon <avrincon1@gmail.com>
2018-03-31 16:23:31 +0200

    Initial upload

---
+++ R/sec_to_hms.R
@@ -0,0 +1,20 @@
+#' Function that converts a number of seconds to a character format hh:m:ss
+#' @param secs a number of seconds to be formatted
+#' @description This function converts a number of seconds to a character format hh:m:ss
+#' @export
+
+sec_to_hms <- function(secs) {
+  # converts a number of seconds to format hh:m:ss
+
+  # integer division to ger the number of full min
+  full.mins <- secs %/% 60
+
+  # change duration format from period to character hh:mm:ss
+  # integer division to get the number of full hours
+  paste(formatC((full.mins %/% 60), width = 2, format = "d", flag = "0"),
+        # remainder of the division is the number of minutes
+        formatC((full.mins %% 60), width = 2, format = "d", flag = "0"),
+        # remainder of the division is the number of secs
+        formatC((secs %% 60), width = 2, format = "d", flag = "0"),
+        sep = ":")
+}
