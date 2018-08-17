842174b8a4684f81e7233230f71eb6c5eafeb84c
Alan Rincon <avrincon1@hotmail.com>
2018-08-17 16:48:11 +0200

    minor edits

--- R/prot_oos_duration_calculator.R
+++ R/prot_oos_duration_calculator.R
@@ -39,8 +39,9 @@ prot_oos_duration_calculator <- function(d_focal,
     if(nrow(dyad.info) > 0 & nrow(dyad.info) %% 2 != 0){
       # error message when dyad does not have equal number of start and end codes
       stop(
-        paste("Number of 'start' codes is NOT equal to the number of 'end' codes.",
-              "Focal =", d.f, ":" ,"Partner =", d.p))
+        paste(
+          "Number of 'start' codes is NOT equal to the number of 'end' codes.",
+          "Focal =", d.f))
 
     } else if (nrow(dyad.info) != 0) {
       # calculate action duration, format to hh:mm:ss
